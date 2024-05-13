44b690bb0efed3edb168ed91892c30b5db5b9c69
Daniel Monfil <davilesmonfil34@gmail.com>
2024-05-13 10:16:38 -0600

    eliminar archivos

--- public/script.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('resize', function() {
-    // Aquí puedes realizar acciones cuando cambia el tamaño de la ventana
-    // Por ejemplo, ajustar dimensiones de elementos o recargar la página
-});
